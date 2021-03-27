5b9afbc14735fde01802806d85b0f7e380941423
rickydodd <rickydoddmobile@gmail.com>
2021-03-28 02:12:13 +1100

    Updated Fuzzifier.py and Fuzzy_Subset_Visualizations.ipynb
    Changed the hedges and shrank change's universe of discourse.

--- Fuzzifier.py
+++ Fuzzifier.py
@@ -7,7 +7,7 @@ angle = ctrl.Antecedent(np.arange(-46, 47, 1), 'angle')
 speed = ctrl.Antecedent(np.arange(0, 101, 1), 'speed')
 
 # Consequent object, to hold universe of discourse and membership functions
-change = ctrl.Consequent(np.arange(-100, 101, 1), 'change')
+change = ctrl.Consequent(np.arange(-50, 51, 1), 'change')
 
 # Fuzzy membership functions for "angle"
 angle['very downhill'] = fuzz.trimf(angle.universe, [-46, -46, -23])
@@ -22,25 +22,27 @@ speed['slow'] = fuzz.trimf(speed.universe, [20, 50, 80])
 speed['fast'] = fuzz.trapmf(speed.universe, [50, 85, 100, 100])
 
 # Fuzzy membership functions for "change"
-change['increase'] = fuzz.smf(change.universe, 0, 100)
-change['constant'] = fuzz.trapmf(change.universe, [-50, -10, 10, 50])
-change['decrease'] = fuzz.zmf(change.universe, -100, 0)
+change['heavily increase'] = fuzz.smf(change.universe, 0, 50)
+change['increase'] = fuzz.trimf(change.universe, [0, 20, 20])
+change['constant'] = fuzz.trapmf(change.universe, [-15, -5, 5, 15])
+change['decrease'] = fuzz.trimf(change.universe, [-20, -20, 0])
+change['heavily decrease'] = fuzz.zmf(change.universe, -50, 0)
 
 # Fuzzy rules
-rule_one        = ctrl.Rule(speed['fast'] & angle['very downhill'],      change['decrease'])
+rule_one        = ctrl.Rule(speed['fast'] & angle['very downhill'],      change['heavily decrease'])
 rule_two        = ctrl.Rule(speed['fast'] & angle['downhill'],           change['decrease'])
 rule_three      = ctrl.Rule(speed['fast'] & angle['flat'],               change['constant'])
 rule_four       = ctrl.Rule(speed['fast'] & angle['uphill'],             change['increase'])
-rule_five       = ctrl.Rule(speed['fast'] & angle['very uphill'],        change['increase'])
+rule_five       = ctrl.Rule(speed['fast'] & angle['very uphill'],        change['heavily increase'])
 
-rule_six        = ctrl.Rule(speed['slow'] & angle['very downhill'],      change['decrease'])
+rule_six        = ctrl.Rule(speed['slow'] & angle['very downhill'],      change['heavily decrease'])
 rule_seven      = ctrl.Rule(speed['slow'] & angle['downhill'],           change['decrease'])
 rule_eight      = ctrl.Rule(speed['slow'] & angle['flat'],               change['constant'])
 rule_nine       = ctrl.Rule(speed['slow'] & angle['uphill'],             change['increase'])
-rule_ten        = ctrl.Rule(speed['slow'] & angle['very uphill'],        change['increase'])
+rule_ten        = ctrl.Rule(speed['slow'] & angle['very uphill'],        change['heavily increase'])
 
-rule_eleven     = ctrl.Rule(speed['very slow'] & angle['very downhill'], change['decrease'])
+rule_eleven     = ctrl.Rule(speed['very slow'] & angle['very downhill'], change['heavily decrease'])
 rule_twelve     = ctrl.Rule(speed['very slow'] & angle['downhill'],      change['decrease'])
 rule_thirteen   = ctrl.Rule(speed['very slow'] & angle['flat'],          change['constant'])
 rule_fourteen   = ctrl.Rule(speed['very slow'] & angle['uphill'],        change['increase'])
-rule_fifteen    = ctrl.Rule(speed['very slow'] & angle['very uphill'],   change['increase'])
+rule_fifteen    = ctrl.Rule(speed['very slow'] & angle['very uphill'],   change['heavily increase'])

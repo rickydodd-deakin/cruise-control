3aa5a341601615dcdb201e72a8a7734d05ea5b2e
rickydodd <rickydoddmobile@gmail.com>
2021-03-27 21:49:07 +1100

    Removed dependencies on Simpful API.
    Removed Simpful, and re-wrote everything in SciKit-Fuzzy, due to SciKit-Fuzzy having better documentation. Simpful may still be useful for inference.

--- Fuzzifier.py
+++ Fuzzifier.py
@@ -1,28 +1,25 @@
 import numpy as np
-import simpful as sf
 import skfuzzy as fuzz
 
-# Universes of discourse
+# Generate universe variables
 x_angle = np.arange(-46, 47, 1)
-x_speed = np.arange(-100, 101, 1)
+x_speed = np.arange(0, 101, 1)
+x_change = np.arange(-100, 101, 1) # Universe for the consequent
 
-# Giving the persistent fuzzy system an easier to handle name
-FuzzySystem = sf.FuzzySystem()
+# Generate fuzzy membership functions
+## Angle (antecedent)
+angle_vd = fuzz.trimf(x_angle, [-46, -46, -23])
+angle_sd = fuzz.trimf(x_angle, [-46, -23, 0])
+angle_f  = fuzz.trimf(x_angle, [-23, 0, 23])
+angle_su = fuzz.trimf(x_angle, [0, 23, 46])
+angle_vu = fuzz.trimf(x_angle, [23, 46, 46])
 
-#  Fuzzy sets
-## Fuzzy sets associated with linguistic variable "angle"
-set_vd = sf.FuzzySet(function=sf.Triangular_MF(a=-46, b=-46, c=-23), term="very downhill")
-set_sd = sf.FuzzySet(function=sf.Triangular_MF(a=-46, b=-23, c=0),   term="somewhat downhill")
-set_f  = sf.FuzzySet(function=sf.Triangular_MF(a=-23, b=0, c=23),    term="flat")
-set_su = sf.FuzzySet(function=sf.Triangular_MF(a=0, b=23, c=46),     term="somewhat uphill")
-set_vu = sf.FuzzySet(function=sf.Triangular_MF(a=23, b=46, c=46),    term="very uphill")
+## Speed (antecedent)
+speed_slo = fuzz.trapmf(x_speed, [0, 0, 15, 50])
+speed_med = fuzz.trimf(x_speed, [20, 50, 80])
+speed_fas = fuzz.trapmf(x_speed, [50, 85, 100, 100])
 
-## Fuzzy sets associated with the linguistic variable "speed"
-set_in  = sf.FuzzySet(function=fuzz.smf(x_speed, 0, 100), term="increase")
-set_con = sf.FuzzySet(function=sf.Trapezoidal_MF())
-
-#  Linguistic variables
-## Linguistic variable "Angle"
-FuzzySystem.add_linguistic_variable("Angle", sf.LinguisticVariable([set_vd, set_sd, set_f, set_su, set_vu],
-                                                                concept="Angle depth", universe_of_discourse=[-46,46]))
-
+## Change (consequent)
+change_inc = fuzz.smf(x_change, 0, 100)
+change_con = fuzz.trapmf(x_change, [-50, -10, 10, 50])
+change_dec = fuzz.zmf(x_change, -100, 0)

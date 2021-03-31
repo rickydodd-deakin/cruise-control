b46f2ebb9e7ed756668c49faf11162731df72cba
patrickwright92 <patrick.wright92@hotmail.com>
2021-03-31 17:34:29 +1100

    Update CruiseControlTester.py
    added more tests - Devin

--- CruiseControlTester.py
+++ CruiseControlTester.py
@@ -24,5 +24,41 @@ def cruiseControlTest(testLength):
         if (time.time() > startTime + testLength):
             Cruise_Control.driving = False
 
-#This starts a test of 10 seconds
-cruiseControlTest(30)
+
+def cruiseControlTest2(testLength):
+    startTime = time.time()
+    
+    Cruise_Control.driving = True
+    maintainingSpeed = False
+    setCCSpeed = 50
+    currentVehicleSpeed = 70
+    frontVehicleDistance = 60
+
+    print("Beginning test of {0} seconds".format(testLength))
+    while (Cruise_Control.driving):
+        setCCSpeed, currentVehicleSpeed, maintainingSpeed = Cruise_Control.Driving(setCCSpeed, currentVehicleSpeed, frontVehicleDistance, maintainingSpeed)
+        print("Set speed is {0}, current vehicle speed is {1}kph, front vehicle is {2}m away".format(setCCSpeed, currentVehicleSpeed, frontVehicleDistance))
+        if (time.time() > startTime + testLength):
+            Cruise_Control.driving = False
+
+
+def cruiseControlTest3(testLength):
+    startTime = time.time()
+    
+    Cruise_Control.driving = True
+    maintainingSpeed = False
+    setCCSpeed = 80
+    currentVehicleSpeed = 80
+    frontVehicleDistance = 90
+
+    print("Beginning test of {0} seconds".format(testLength))
+    while (Cruise_Control.driving):
+        setCCSpeed, currentVehicleSpeed, maintainingSpeed = Cruise_Control.Driving(setCCSpeed, currentVehicleSpeed, frontVehicleDistance, maintainingSpeed)
+        print("Set speed is {0}, current vehicle speed is {1}kph, front vehicle is {2}m away".format(setCCSpeed, currentVehicleSpeed, frontVehicleDistance))
+        if (time.time() > startTime + testLength):
+            Cruise_Control.driving = False
+
+#This starts a test of 30 seconds
+#cruiseControlTest(30)
+#cruiseControlTest2(30)
+#cruiseControlTest3(30)
